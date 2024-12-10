3f0d366e5dd905c06ea9c67ac8189a9077c18767
Sebastian Moretto Krog <sebastian.krog@gmail.com>
2024-12-10 16:46:18 +0100

    Failed attempt at Day9 part2

--- advent2024_D9-16.R
+++ advent2024_D9-16.R
@@ -23,8 +23,7 @@ d9_df <- tibble(count = d9, checksum_pos = d9_positions) %>%
   ) %>% ungroup() %>% filter(count != 0) %>% rowwise() %>% 
   mutate(
     checksum_pos = list(checksum_pos:(checksum_pos+count-1))
-  ) %>% unnest(c(checksum_pos)) %>% 
-  select(checksum_pos, data, id)
+  ) %>% unnest(c(checksum_pos))
 
 d9_data <- d9_df %>% filter(data == T) %>% arrange(desc(id))
 
@@ -41,3 +40,69 @@ for(i in 1:nrow(d9_data)) {
 
 d9_1_answer <- checksum
 
+# Part 2
+
+d9_2_df <- d9_df %>% 
+  mutate(
+    moved = F,
+    pos = row_number()
+  )
+
+reformat_1 <- function(df) {
+  ret_df <- df
+  
+  # Find next data to move
+  next_data <- df %>% filter(data==T, moved==F) %>% filter(id == max(id))
+  
+  if (nrow(next_data) == 0) {
+    return(list(df=ret_df, moved=F))
+  }
+  
+  next_pos <- min(next_data$pos)
+  next_count <- next_data$count[[1]]
+  next_id <- next_data$id[[1]]
+  
+  # Find first slot to move to
+  slot <- df %>% filter(data==F, pos < next_pos, count >= next_count) %>% 
+    arrange(pos) %>% 
+    slice_head(n=next_count)
+  
+  if (nrow(slot) > 0) {
+    # Remove old data
+    ret_df$id[next_data$pos] <- -1
+    ret_df$data[next_data$pos] <- F
+    
+    # Add new data
+    ret_df$id[slot$pos] <- next_id
+    ret_df$data[slot$pos] <- T
+    ret_df$moved[slot$pos] <- T
+    
+    # Recalculate counts
+    ret_df <- ret_df %>% group_by(data, id) %>% mutate(
+      count = n()
+    ) %>% ungroup()
+  } else {
+    ret_df$moved[next_data$pos] <- T
+  }
+  
+  return(list(df=ret_df, moved=T))
+}
+
+d9_2_out <- d9_2_df
+moved <- T
+cli::cli_progress_bar("Reformatting...", total=max(df$id)+1)
+while(moved) {
+  attempt_move <- reformat_1(d9_2_out)
+  d9_2_out <- attempt_move$df
+  moved <- attempt_move$moved
+  cli::cli_progress_update()
+}
+
+d9_2_calc_df <- d9_2_out
+
+d9_2_calc_df <- d9_2_calc_df %>% mutate(
+  check_pos = row_number() -1,
+  check_val = id*check_pos
+)
+
+d9_2_answer <- d9_2_calc_df %>% filter(data==F) %>% pull(check_val) %>% sum()
