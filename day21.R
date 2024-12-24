d0ee0285a128f673255a7a6299b2398f34466ac3
Sebastian Moretto Krog <sebastian.krog@gmail.com>
2024-12-24 09:33:34 +0100

    Day 21 part 2 done

--- day21.R
+++ day21.R
@@ -27,7 +27,8 @@ dir_pad_edge_path <- function(src, dst) {
       "<" = c("v", "<", "<", "A"),
       ">" = c("v", "A"),
       "^" = c("<", "A"),
-      "v" = c("v", "<", "A")
+      #"v" = c("v", "<", "A")
+      "v" = c("<", "v", "A")
     )
   } else if (src == "<") {
     switch (dst,
@@ -52,7 +53,8 @@ dir_pad_edge_path <- function(src, dst) {
     )
   } else if (src == "v") {
     switch (dst,
-      "A" = c(">", "^", "A"),
+      #"A" = c(">", "^", "A"),
+      "A" = c("^", ">", "A"),
       "<" = c("<", "A"),
       ">" = c(">", "A"),
       "^" = c("^", "A")
@@ -140,7 +142,7 @@ answer_1 <- sum(unlist(map(codes, complexity)) * num_input)
 
 
 # Part 2
-#calc_answer_2 <- function() {
+calc_answer_2 <- function() {
   dir_pad_edge_level <- memoise(function(src, dst, level) {
     path <- dir_pad_edge_path(src, dst)
     if (level == 1) return(length(path))
@@ -154,7 +156,12 @@ answer_1 <- sum(unlist(map(codes, complexity)) * num_input)
   
   answer_2 <- sum(unlist(map(codes, complexity_level)) * num_input)
   answer_2
-#}
+}
+
+answer_2 <- calc_answer_2()
+answer_2 |> format(scientific = F)
+
+answer_2 < 218309335714068
 
 # NOT 349855514937220 -- too high
 # NOT 137813495802036 -- too low
